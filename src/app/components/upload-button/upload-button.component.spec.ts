ec3e5358a18b94c546d426a02a3cddf1eec5165d
eawetchy <elisabeth.wetchy@gmail.com>
2022-05-14 17:10:40 +0100

    EL-25 Unit tests

--- src/app/components/upload-button/upload-button.component.spec.ts
+++ src/app/components/upload-button/upload-button.component.spec.ts
@@ -21,4 +21,44 @@ describe('UploadButtonComponent', () => {
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should react to file upload field change', () => {
+        const element = fixture.nativeElement;
+        const input = element.querySelector('.file-input');
+        spyOn(component, 'handleFileInput');
+
+        input.dispatchEvent(new Event('change'));
+
+        expect(component.handleFileInput).toHaveBeenCalled();
+    });
+
+    it('should emit event with file content', () => {
+        const element = fixture.nativeElement;
+        const input = element.querySelector('.file-input');
+        const dataTransfer = new DataTransfer()
+        dataTransfer.items.add(new File([exampleFileContent], 'test-file.txt'));
+        input.files = dataTransfer.files;
+
+        component.newFileUploadedEvent.subscribe((fileContent: string) => {
+            expect(fileContent).toBe(exampleFileContent);
+        })
+        input.dispatchEvent(new Event('change'));
+    });
+
+    it('should throw warning if file is not .txt', () => {
+        const element = fixture.nativeElement;
+        const input = element.querySelector('.file-input');
+
+        const dataTransfer = new DataTransfer()
+        dataTransfer.items.add(new File([''], 'test-file.xes'))
+        input.files = dataTransfer.files;
+
+        spyOn(window, "alert");
+
+        input.dispatchEvent(new InputEvent('change'));
+
+        expect(window.alert).toHaveBeenCalledWith('Nur .txt-Dateien vom Typ .log werden derzeit unterstützt');
+    });
+
+    const exampleFileContent = '.type log\n.events\ne1 a\ne2 b\n.arcs\ne1 2';
 });
