9ee96c4236701278b4fa85405f04cd824e7a055a
yannikworthmann <58826780+yannikworthmann@users.noreply.github.com>
2022-06-09 21:28:29 +0200

    improvements

--- src/app/services/eventlog-data.service.ts
+++ src/app/services/eventlog-data.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@angular/core';
-import { Event } from '../classes/EventLog/event';
 import { EventLog } from '../classes/EventLog/eventlog';
-import { Trace } from '../classes/EventLog/trace';
-import { ParserService } from './parser.service';
 
 @Injectable({
     providedIn: 'root',
 })
 export class EventlogDataService {
-    private _EventLog: EventLog = new EventLog([], [], [], [], []); //
+    private _EventLog: EventLog;
 
     public get eventLog(): EventLog {
         return this._EventLog;
     }
+
     public set eventLog(value: EventLog) {
         this._EventLog = value;
     }
 
-    constructor() {}
+    constructor() {
+        this._EventLog = new EventLog([], [], [], [], []);
+    }
 }
