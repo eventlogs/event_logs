8f632e830e15a655bc6e73d40ad46dd6a20ec429
ThomasKaputa <54510958+ThomasKaputa@users.noreply.github.com>
2022-06-14 19:08:58 +0200

    El 37 lange activity strings richtig anzeigen (#15)
    * Erster Versuch für Monospace Fonts
    
    * Alle Fonts unterstützen
    
    * farben geändert / logging entfernt
    
    * linter error noch behoben
    
    * Substring Index angepasst
    
    * fix linter error
    
    Co-authored-by: yannikworthmann <58826780+yannikworthmann@users.noreply.github.com>

--- src/app/services/svg.service.ts
+++ src/app/services/svg.service.ts
@@ -7,17 +7,22 @@ import { Element, ElementType } from '../classes/diagram/element';
 })
 export class SvgService {
     private readonly backgroundColors = [
-        'rgb(206, 57, 57)',
-        'rgb(210, 55, 89)',
-        'rgb(205, 62, 119)',
-        'rgb(193, 76, 146)',
-        'rgb(175, 91, 169)',
-        'rgb(151, 105, 185)',
-        'rgb(125, 117, 195)',
-        'rgb(97, 128, 199)',
-        'rgb(71, 136, 196)',
-        'rgb(54, 142, 189)',
+        'rgb(227, 26, 28)',
+        'rgb(251, 154, 153)',
+        'rgb(254, 191, 111)',
+        'rgb(166, 206, 227)',
+        'rgb(32, 120, 181)',
+        'rgb(125, 227, 183)',
+        'rgb(152, 250, 232, 1)',
+        'rgb(112, 203, 255)',
+        'rgb(227, 175, 166)',
+        'rgb(181, 65, 33)',
     ];
+    private readonly FONT = '15px sans-Serif';
+    private readonly MAXFONTWIDTH = 120;
+    private readonly XTEXTOFFSET = 5;
+    private readonly YTEXTOFFSET = 4;
+    private readonly YNEXTROWOFFSET = 20;
 
     private readonly xTraceBorderOffset = -5;
     private readonly yTraceBorderOffset = -24;
@@ -58,7 +63,7 @@ export class SvgService {
                 ev.svgElements.forEach(svgEl => {
                     if (svgEl.type === ElementType.text) {
                         const text = this.createSvgForText(
-                            ev.svgElements[0],
+                            ev.svgElements[1],
                             ev.activity
                         );
                         result.push(text);
@@ -71,7 +76,7 @@ export class SvgService {
                             ); // Wenn die Map größer ist als Size Farben recyclen
                         }
                         const rect = this.createBoxForElement(
-                            ev.svgElements[1],
+                            ev.svgElements[0],
                             this.activityColorMap.get(ev.activity)!
                         );
                         result.push(rect);
@@ -111,14 +116,58 @@ export class SvgService {
         return svg;
     }
 
+    private getSubStrings(text: String): [String, String] {
+        for (let i = 0; i <= text.length; i++) {
+            let length = this.GetStringWidth(text.substring(0, i));
+            if (length > this.MAXFONTWIDTH) {
+                for (let j = i; j <= text.length; j++) {
+                    let length2 = this.GetStringWidth(text.substring(i, j - 1));
+                    if (length2 > this.MAXFONTWIDTH) {
+                        return [
+                            text.substring(0, i),
+                            text.substring(i, j - 2) + '...',
+                        ];
+                    }
+                }
+                return [text.substring(0, i), text.substring(i)];
+            }
+        }
+        return [text, ''];
+    }
+
     private createSvgForText(element: Element, text: String): SVGElement {
         const svg = this.createSvgElement('text');
-        svg.setAttribute('x', `${element.x}`);
-        svg.setAttribute('y', `${element.y}`);
-        svg.setAttribute('font', 'bold 30px sans-serif');
-        svg.textContent = text.toString();
+        svg.setAttribute('x', `${element.x - this.XTEXTOFFSET}`);
+        svg.setAttribute('y', `${element.y - this.YTEXTOFFSET}`);
+        let stringWidth = this.GetStringWidth(text);
+        if (stringWidth > this.MAXFONTWIDTH) {
+            let subStrings = this.getSubStrings(text);
+            const svg1 = this.createSvgElement('tspan');
+            svg1.setAttribute('font', this.FONT);
+            svg1.textContent = subStrings[0].toString();
+            const svg2 = this.createSvgElement('tspan');
+            svg2.setAttribute('font', this.FONT);
+            svg2.textContent = subStrings[1].toString();
+            svg2.setAttribute('x', `${element.x - this.XTEXTOFFSET}`);
+            svg2.setAttribute('dy', `${this.YNEXTROWOFFSET}` + 'px');
+            svg.appendChild(svg1);
+            svg.appendChild(svg2);
+        } else {
+            svg.setAttribute('font', this.FONT);
+            svg.textContent = text.toString();
+        }
         element.registerSvg(svg);
         return svg;
+    }
+
+    private GetStringWidth(text: String): number {
+        var canvas = document.createElement('canvas');
+        canvas.setAttribute('width', '100%');
+        canvas.setAttribute('height', '380px');
+        var ctx = canvas.getContext('2d');
+        ctx!.font = this.FONT;
+        var strWidth = ctx!.measureText(text.toString()).width;
+        return strWidth;
     }
 
     private createSvgElement(name: string): SVGElement {
