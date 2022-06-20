81570e64e7f757c7b9fa0dc0b041c68c4b29454f
eawetchy <elisabeth.wetchy@gmail.com>
2022-06-20 14:58:21 +0200

    Tests korrekt initialisiert und Fehler durch korrekten Lifecycle-Hook behoben

--- src/app/components/drawingArea/drawingArea.component.spec.ts
+++ src/app/components/drawingArea/drawingArea.component.spec.ts
@@ -1,9 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { DrawingAreaComponent } from './drawingArea.component';
+import { WertschoepfungsketteComponent } from '../wertschoepfungskette/wertschoepfungskette.component';
+import { SwitchViewButtonComponent } from '../switch-view-button/switch-view-button.component';
+import { DirectlyFollowsGraphComponent } from '../directly-follows-graph/directly-follows-graph.component';
 
 describe('DrawingAreaComponent', () => {
     let component: DrawingAreaComponent;
     let fixture: ComponentFixture<DrawingAreaComponent>;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [
+                DrawingAreaComponent,
+                SwitchViewButtonComponent,
+                WertschoepfungsketteComponent,
+                DirectlyFollowsGraphComponent,
+            ],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DrawingAreaComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
     it('should create', () => {
         expect(component).toBeTruthy();
