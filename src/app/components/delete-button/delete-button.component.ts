e2cb2a36e28023cc8ae5667cd2765fb04d1132b1
yannikworthmann <58826780+yannikworthmann@users.noreply.github.com>
2022-06-23 13:48:40 +0200

    update textarea after deletion of a trace (#18)

--- src/app/components/delete-button/delete-button.component.ts
+++ src/app/components/delete-button/delete-button.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { DisplayService } from 'src/app/services/display.service';
 import { EventlogDataService } from 'src/app/services/eventlog-data.service';
+import { LogService } from 'src/app/services/log.service';
 
 @Component({
     selector: 'app-delete-button',
@@ -10,10 +11,12 @@ import { EventlogDataService } from 'src/app/services/eventlog-data.service';
 export class DeleteButtonComponent {
     @Input() buttonText: string | undefined;
     @Input() buttonIcon: string | undefined;
+    @Output() updateTextarea = new EventEmitter();
 
     constructor(
         private _displayService: DisplayService,
-        private _eventlogDataService: EventlogDataService
+        private _eventlogDataService: EventlogDataService,
+        private _logService: LogService
     ) {}
 
     processMouseClick(e: MouseEvent) {
@@ -22,5 +25,7 @@ export class DeleteButtonComponent {
             this._displayService.selectedTraceCaseIds
         );
         this._displayService.selectTraceCaseIds([]);
+        let logString = this._logService.generate(eventLog);
+        this.updateTextarea.emit(logString);
     }
 }
