24c8f8f4278cad1900509c4621e42896d0dad367
yannikworthmann <58826780+yannikworthmann@users.noreply.github.com>
2022-07-25 14:52:50 +0200

    EL-78: new example log

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -37,10 +37,8 @@ export class AppComponent implements OnDestroy {
         this._sub = this.textareaFc.valueChanges
             .pipe(debounceTime(400))
             .subscribe(val => this.processSourceChange(val));
-        this.textareaFc.setValue(this.textareaExampleValue());
-        this._eventlogDataService.eventLog = this._logParserService.parse(
-            this.textareaExampleValue()
-        );
+
+        this.processXesImport(this.xesExampleValue());
 
         this._subSelectedTraces =
             this._displayService.selectedTraceCaseIds$.subscribe(
@@ -140,25 +138,6 @@ export class AppComponent implements OnDestroy {
         return this.textareaFc.value;
     }
 
-    textareaExampleValue() {
-        return (
-            '.type log\n' +
-            '.attributes\n' +
-            'case-id\n' +
-            'concept:name\n' +
-            'booleanValue\n' +
-            'intValue\n' +
-            'floatValue\n' +
-            'dateValue\n' +
-            'stringValue\n' +
-            '.events\n' +
-            '1 Auto true 1 1.3 2020-01-31 basadf\n' +
-            '1 Schiff true 2 2.3 2020-01-31 dasf\n' +
-            '2 BusBus false 3 4.5 2020-01-31 adsf\n' +
-            '2 Bus false 4 6.7 2020-01-31 adfd'
-        );
-    }
-
     updateViews() {
         this._displayService.displayEventLog(
             this._eventlogDataService.eventLog
@@ -173,5 +152,257 @@ export class AppComponent implements OnDestroy {
         }
 
         this.tracesDetailView?.refresh();
+    }
+
+    xesExampleValue() {
+        return (
+            '<?xml version="1.0" encoding="UTF-8"?>' +
+            '<log xes.version="1.0" xes.features="nested-attributes">' +
+            '<extension name="Lifecycle" prefix="lifecycle" uri="http://www.xes-standard.org/lifecycle.xesext"/>' +
+            '<extension name="Organizational" prefix="org" uri="http://www.xes-standard.org/org.xesext"/>' +
+            '<extension name="Time" prefix="time" uri="http://www.xes-standard.org/time.xesext"/>' +
+            '<extension name="Concept" prefix="concept" uri="http://www.xes-standard.org/concept.xesext"/>' +
+            '<extension name="Semantic" prefix="semantic" uri="http://www.xes-standard.org/semantic.xesext"/>' +
+            '<global scope="trace">' +
+            '<string key="concept:name" value="__INVALID__"/>' +
+            '</global>' +
+            '<global scope="event">' +
+            '<string key="concept:name" value="__INVALID__"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '</global>' +
+            '<classifier name="MXML_Legacy_Classifier" keys="concept:name lifecycle:transition"/>' +
+            '<classifier name="Event_Name" keys="concept:name"/>' +
+            '<classifier name="Resource" keys="org:resource"/>' +
+            '<classifier name="Event_Name_AND_Resource" keys="concept:name org:resource"/>' +
+            '<string key="source" value="CPN Tools simulation"/>' +
+            '<string key="concept:name" value="repairExample.mxml"/>' +
+            '<string key="lifecycle:model" value="standard"/>' +
+            '<string key="description" value="Simulated process"/>' +
+            '<trace>' +
+            '<string key="concept:name" value="1"/>' +
+            '<string key="description" value="Simulated process instance"/>' +
+            '<event>' +
+            '<string key="concept:name" value="Register"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:23:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="org:resource" value="Tester3"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:23:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="defectType" value="6"/>' +
+            '<string key="org:resource" value="Tester3"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="phoneType" value="T2"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:30:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Complex)"/>' +
+            '<string key="org:resource" value="SolverC1"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:31:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Complex)"/>' +
+            '<string key="org:resource" value="SolverC1"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:49:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="org:resource" value="Tester3"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:49:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="numberRepairs" value="0"/>' +
+            '<string key="org:resource" value="Tester3"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-02T12:55:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Inform User"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-02T13:10:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Archive Repair"/>' +
+            '<string key="numberRepairs" value="0"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-02T13:10:00.000Z"/>' +
+            '</event>' +
+            '</trace>' +
+            '<trace>' +
+            '<string key="concept:name" value="10"/>' +
+            '<string key="description" value="Simulated process instance"/>' +
+            '<event>' +
+            '<string key="concept:name" value="Register"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:09:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="org:resource" value="Tester2"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:09:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="defectType" value="3"/>' +
+            '<string key="org:resource" value="Tester2"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="phoneType" value="T1"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:15:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Simple)"/>' +
+            '<string key="org:resource" value="SolverS1"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:35:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Simple)"/>' +
+            '<string key="org:resource" value="SolverS1"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:42:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="org:resource" value="Tester6"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:42:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="numberRepairs" value="1"/>' +
+            '<string key="org:resource" value="Tester6"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="false"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:48:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Restart Repair"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:54:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Simple)"/>' +
+            '<string key="org:resource" value="SolverS2"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:54:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Inform User"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-01T11:55:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Simple)"/>' +
+            '<string key="org:resource" value="SolverS2"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-01T12:03:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="org:resource" value="Tester4"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-01T12:03:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="numberRepairs" value="2"/>' +
+            '<string key="org:resource" value="Tester4"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-01T12:09:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Archive Repair"/>' +
+            '<string key="numberRepairs" value="2"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-01T12:14:00.000Z"/>' +
+            '</event>' +
+            '</trace>' +
+            '<trace>' +
+            '<string key="concept:name" value="100"/>' +
+            '<string key="description" value="Simulated process instance"/>' +
+            '<event>' +
+            '<string key="concept:name" value="Register"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-04T02:28:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="org:resource" value="Tester4"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-04T02:28:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Analyze Defect"/>' +
+            '<string key="defectType" value="8"/>' +
+            '<string key="org:resource" value="Tester4"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="phoneType" value="T2"/>' +
+            '<date key="time:timestamp" value="1970-01-04T02:36:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Complex)"/>' +
+            '<string key="org:resource" value="SolverC1"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-04T02:52:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Repair (Complex)"/>' +
+            '<string key="org:resource" value="SolverC1"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-04T03:09:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="org:resource" value="Tester1"/>' +
+            '<string key="lifecycle:transition" value="start"/>' +
+            '<date key="time:timestamp" value="1970-01-04T03:09:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Test Repair"/>' +
+            '<string key="numberRepairs" value="0"/>' +
+            '<string key="org:resource" value="Tester1"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-04T03:18:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Inform User"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<date key="time:timestamp" value="1970-01-04T03:20:00.000Z"/>' +
+            '</event>' +
+            '<event>' +
+            '<string key="concept:name" value="Archive Repair"/>' +
+            '<string key="numberRepairs" value="0"/>' +
+            '<string key="org:resource" value="System"/>' +
+            '<string key="lifecycle:transition" value="complete"/>' +
+            '<string key="defectFixed" value="true"/>' +
+            '<date key="time:timestamp" value="1970-01-04T03:28:00.000Z"/>' +
+            '</event>' +
+            '</trace>' +
+            '</log>'
+        );
     }
 }
