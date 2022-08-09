5720392b03af3a7abc2af164477b7cc3a6c0089d
eawetchy <elisabeth.wetchy@gmail.com>
2022-08-09 22:28:04 +0100

    EL-85 Tests

--- src/app/services/views/value-chain/value-chain-controller.service.spec.ts
+++ src/app/services/views/value-chain/value-chain-controller.service.spec.ts
@@ -1,12 +1,41 @@
 import { TestBed } from '@angular/core/testing';
-
 import { ValueChainControllerService } from './value-chain-controller.service';
+import { LayoutService } from "../../common/layout-service/layout.service";
+import { SvgService } from "../../common/svg-service/svg.service";
 
 describe('ValueChainControllerService', () => {
   let service: ValueChainControllerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+        providers: [
+            {
+                provide: LayoutService.VALUE_CHAIN_INSTANCE,
+                useFactory: () =>
+                    new LayoutService(
+                        LayoutService.X_OFFSET_VALUE_CHAIN,
+                        LayoutService.Y_OFFSET_VALUE_CHAIN,
+                        LayoutService.X_STEP_VALUE_CHAIN,
+                        LayoutService.Y_STEP_VALUE_CHAIN,
+                        LayoutService.X_LABELSIZE_VALUE_CHAIN
+                    ),
+            },
+            {
+                provide: SvgService.VALUE_CHAIN_INSTANCE,
+                useFactory: (layoutService: LayoutService) =>
+                    new SvgService(
+                        layoutService,
+                        SvgService.MAXFONTWIDTH_VALUE_CHAIN,
+                        SvgService.BOX_WIDTH_VALUE_CHAIN,
+                        SvgService.BOX_HEIGHT_VALUE_CHAIN,
+                        trace =>
+                            trace.count.toString() +
+                            (trace.count == 1 ? ' trace' : ' traces')
+                    ),
+                deps: [LayoutService.VALUE_CHAIN_INSTANCE],
+            }
+        ]
+    });
     service = TestBed.inject(ValueChainControllerService);
   });
 
